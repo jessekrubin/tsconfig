1ab5d24b9a2a3b64612f684a0a82dac5783977c2
jessekrubin <jessekrubin@gmail.com>
2026-03-20 17:03:10 -0700

    more cleanup

--- vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defaultExclude, defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    globals: false,
-    exclude: [...defaultExclude, "_fixtures/**", "fixtures/**"],
-  },
-});
